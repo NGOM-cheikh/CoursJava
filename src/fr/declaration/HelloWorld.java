e37eec5aa9638cfe768b5b37cc2da085e442bfb2
cheikh19 <mourtalla211@gmail.com>
2024-07-12 12:30:39 +0200

    version1.0

---
+++ src/fr/declaration/HelloWorld.java
@@ -0,0 +1,9 @@
+package fr.declaration;
+
+import javax.xml.transform.stream.StreamSource;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world !") ;
+    }
+}
---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,22 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args){
+    // Déclaration et initialisation des variables
+    byte byteVariable = 10;
+    short shortVariable = 20;
+    int intVariable = 30;
+    long longVariable = 40000L;
+
+
+    // Affichage des valeurs des variables
+        System.out.println("Valeur de byteVariable: " + byteVariable);
+        System.out.println("Valeur de shortVariable: " + shortVariable);
+        System.out.println("Valeur de intVariable: " + intVariable);
+        System.out.println("Valeur de longVariable: " + longVariable);
+
+        String randomString = "Voici le résultat d’un calcul : 1+5=6";
+         System.out.println("Voici le résultat d’un calcul : 1+5=6");
+   }
+
+}
